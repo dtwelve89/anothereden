52c3407797c5515374a36284d943ffcb5cace847
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-09-15 13:49:26 -0700

    Added flip card effect

--- src/components/layout/CharacterItem.js
+++ src/components/layout/CharacterItem.js
@@ -2,20 +2,30 @@ import React from 'react';
 import './CharacterItem.css';
 
 const CharacterItem = ({ character }) => {
+  // const flipItem = () => {
+  //   let itemInner = document.querySelector('.item-inner');
+  //   itemInner.classList.add('flip-item');
+  // };
+
   return (
     <figure className='item'>
-      <img src={character.img} alt='Character' />
-      <div className='character-description'>
-        <h2 className='text-center text-uppercase'>{character.name}</h2>
-        <ul>
-          {/* <li>Rarity: {character.rarity}</li>
-          <li>Element: {character.element}</li>
-          <li>Attack Type: {character.attackType}</li>
-          <li>Weapon Type: {character.weaponType}</li>
-          <li>Accessory Type: {character.accessoryType}</li>
-          <li>Light/Shadow Type: {character.lightShadowType}</li> */}
-          {/* <li>Personalities: {character.personalities}</li> */}
-        </ul>
+      <div className='item-inner'>
+        <div className='item-front'>
+          <img src={character.img} alt='Character' />
+          <h2 className='text-center text-uppercase'>{character.name}</h2>
+        </div>
+        <div className='item-back'>
+          <ul>
+            <h2 className='text-center text-uppercase'>{character.name}</h2>
+            <li>Rarity: {character.rarity}</li>
+            <li>Element: {character.element}</li>
+            <li>Attack Type: {character.attackType}</li>
+            <li>Weapon Type: {character.weaponType}</li>
+            <li>Accessory Type: {character.accessoryType}</li>
+            <li>Light/Shadow Type: {character.lightShadowType}</li>
+            {/* <li>Personalities: {character.personalities}</li> */}
+          </ul>
+        </div>
       </div>
     </figure>
   );
