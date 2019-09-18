e7f19158c31cd2d20a31e3868f9d51f6d3a31f87
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-09-18 13:03:56 -0700

    Character update and delete routes

--- routes/characters.js
+++ routes/characters.js
@@ -21,22 +21,126 @@ router.get('/', auth, async (req, res) => {
 // @route       POST /api/characters
 // @desc        Add new character
 // @access      Private
-router.post('/', (req, res) => {
-  res.send('Add character');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      name,
+      img,
+      rarity,
+      element,
+      attackType,
+      weaponType,
+      accessoryType,
+      lightShadowType
+    } = req.body;
+
+    try {
+      const newCharacter = new Character({
+        name,
+        img,
+        rarity,
+        element,
+        attackType,
+        weaponType,
+        accessoryType,
+        lightShadowType,
+        user: req.user.id
+      });
+
+      const character = await newCharacter.save();
+
+      res.json(character);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route       PUT /api/characters/:id
 // @desc        Update character
 // @access      Private
-router.put('/', (req, res) => {
-  res.send('Update character');
+router.put('/:id', auth, async (req, res) => {
+  const {
+    name,
+    img,
+    rarity,
+    element,
+    attackType,
+    weaponType,
+    accessoryType,
+    lightShadowType
+  } = req.body;
+
+  // Build character object
+  const characterFields = {};
+  if (name) characterFields.name = name;
+  if (img) characterFields.img = img;
+  if (rarity) characterFields.rarity = rarity;
+  if (element) characterFields.element = element;
+  if (attackType) characterFields.attackType = attackType;
+  if (weaponType) characterFields.weaponType = weaponType;
+  if (accessoryType) characterFields.accessoryType = accessoryType;
+  if (lightShadowType) characterFields.lightShadowType = lightShadowType;
+
+  try {
+    let character = await Character.findById(req.params.id);
+
+    if (!character) return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns the character
+    if (character.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    character = await Character.findByIdAndUpdate(
+      req.params.id,
+      { $set: characterFields },
+      { new: true }
+    );
+
+    res.json(character);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route       DELETE /api/characters/:id
 // @desc        Delete character
 // @access      Private
-router.delete('/', (req, res) => {
-  res.send('Delete character');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let character = await Character.findById(req.params.id);
+
+    if (!character) return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns the character
+    if (character.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Character.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Character removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
